449f09f5179d3c50ee553b1f240ab44a04c14cd0
unknown <mikov.ns.04@gmail.com>
2024-09-13 21:23:24 +0300

    added question

--- helloworld.rb
+++ helloworld.rb
@@ -1,1 +1,15 @@
-puts "Hello Wolrd!!"
+puts "Привет, " + ARGV[0]
+puts "Введите ваш любимый язык программирования: "
+user_language = $stdin.gets.chomp
+
+if user_language == "ruby"
+    puts "Не подлизывайся!"
+else
+    case user_language
+        when "PHP" then puts "Смерть..."
+        when "javascript" then puts "1+1=11?" 
+        when "html" then puts "Это не язык программирования"
+        when "GO" then puts "Фу,зумер что ли?"    
+    end
+    puts "Ладно, ты все равно будешь любить только ruby"
+end
