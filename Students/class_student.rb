f1cc064c29a2544fc2e1b1073dbbc514a918990e
inbento <mikov.ns.04@gmail.com>
2024-09-27 21:56:14 +0300

    (lab2 task 1.9)Added validation for git and other contacts

--- Students/class_student.rb
+++ Students/class_student.rb
@@ -1,38 +1,31 @@
 class Student
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :git
 
-  # Валидация телефона
   def self.valid_phone?(phone)
     !!(phone =~ /\A(\+7|8)\d{10}\z/)
   end
 
-  # Преобразуем номер, начинающийся с 8, в формат +7
   def self.normalize_phone(phone)
     phone.sub(/\A8/, '+7')
   end
 
-  # Валидация фамилии, имени и отчества
   def self.valid_name?(name)
     !!(name =~ /\A[А-Яа-яЁё]{2,50}\z/)
   end
 
-  # Валидация Telegram (начинается с @, содержит буквы, цифры и подчеркивание)
   def self.valid_telegram?(telegram)
     !!(telegram =~ /\A@[a-zA-Z0-9_]{5,}\z/)
   end
 
-  # Валидация email
   def self.valid_email?(email)
     !!(email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
   end
 
-  # Валидация GitHub (простейшая проверка на URL)
   def self.valid_git?(git)
     !!(git =~ /\Ahttps?:\/\/github\.com\/[\w\-]+\z/)
   end
 
   def initialize(last_name, first_name, middle_name, options = {})
-    # Валидация ФИО
     raise ArgumentError, "Некорректная фамилия: #{last_name}" unless Student.valid_name?(last_name)
     raise ArgumentError, "Некорректное имя: #{first_name}" unless Student.valid_name?(first_name)
     raise ArgumentError, "Некорректное отчество: #{middle_name}" unless Student.valid_name?(middle_name)
@@ -42,7 +35,6 @@ class Student
     @middle_name = middle_name
     @id = options[:id]
 
-    # Валидация и нормализация номера телефона
     if options[:phone]
       if Student.valid_phone?(options[:phone])
         @phone = Student.normalize_phone(options[:phone])
@@ -51,23 +43,37 @@ class Student
       end
     end
 
-    # Валидация Telegram
     if options[:telegram]
       raise ArgumentError, "Некорректный Telegram: #{options[:telegram]}" unless Student.valid_telegram?(options[:telegram])
       @telegram = options[:telegram]
     end
 
-    # Валидация email
     if options[:email]
       raise ArgumentError, "Некорректный email: #{options[:email]}" unless Student.valid_email?(options[:email])
       @email = options[:email]
     end
 
-    # Валидация GitHub
     if options[:git]
       raise ArgumentError, "Некорректный GitHub: #{options[:git]}" unless Student.valid_git?(options[:git])
       @git = options[:git]
     end
+
+    validate
+  end
+
+  def validate_contact
+    if !@phone && !@telegram && !@email
+      raise ArgumentError, "Должен быть хотя бы один контакт для связи: телефон, Telegram или email."
+    end
+  end
+
+  def validate_git
+    raise ArgumentError, "Не указан GitHub аккаунт." unless @git
+  end
+  
+  def validate
+    validate_contact
+    validate_git
   end
 
   def to_s
--- Students/main.rb
+++ Students/main.rb
@@ -3,22 +3,19 @@ require_relative 'class_student'
 begin
   student1 = Student.new("Иванов", "Иван", "Иванович", {
     id: 1,
-    phone: "+79991234567", # корректный телефон
-    telegram: "@ivanov",   # корректный telegram
-    email: "ivanov@example.com", # корректный email
-    git: "https://github.com/ivanov" # корректный github
+    phone: "+79991234567",
+    telegram: "@ivanov", 
+    email: "ivanov@example.com",
+    git: "https://github.com/ivanov"
   })
-  
+
   puts student1
+  puts
   student2 = Student.new("Петров", "Петр", "Петрович", {
     id: 2,
-    phone: "89991234568",  # корректный телефон с 8
-    telegram: "petrov",    # некорректный telegram, должен быть с @
-    email: "petrov@com",   # некорректный email
-    git: "github.com/petrov" # некорректный URL для GitHub
+    phone: "89991234568",
   })
-  
-  puts
+
   puts student2
 
 rescue ArgumentError => e
