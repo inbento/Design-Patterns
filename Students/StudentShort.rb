987aa1b6b016a8f9ead200489ad08642097ee0d8
inbento <mikov.ns.04@gmail.com>
2024-10-19 10:34:43 +0300

    (lab2) Fix StudentShort or Not?

--- Students/StudentShort.rb
+++ Students/StudentShort.rb
@@ -9,17 +9,14 @@ class StudentShort < StudentBase
       @surname_with_initials = arg1.surname_with_initials
     elsif arg1.is_a?(Integer) && arg2.is_a?(String)
       super(id: arg1)
-      parse_info(arg2)
+      info_string = arg2
+      parts = info_string.split(';')
+      @surname_with_initials = parts[0].strip
+      @git = parts[1].split(':').last.strip
+      @contact = parts[2].split(':').last.strip
     else
       raise ArgumentError, "Неверные аргументы для инициализации"
     end
-  end
-
-  def parse_info(info_string)
-    parts = info_string.split(';')
-    @surname_with_initials = parts[0].strip
-    @git = parts[1].split(':').last.strip
-    @contact = parts[2].split(':').last.strip
   end
 
   def to_s
