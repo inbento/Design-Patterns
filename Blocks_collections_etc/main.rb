0f681e5b1ad2ca18ae6eb1494856e689ae227f70
inbento <mikov.ns.04@gmail.com>
2024-11-01 19:53:15 +0300

    (lab3 task 1.12)Added reverse between

---
+++ Blocks_collections_etc/main.rb
@@ -0,0 +1,64 @@
+def reverse_between_min_and_max(arr)
+
+  min_index, max_index = arr.index(arr.min), arr.index(arr.max)
+
+  min_index, max_index = [min_index, max_index].sort
+
+  yield(min_index, max_index)
+
+end
+
+
+puts "Выберите способ ввода данных:
+      \n1. Ввести с клавиатуры
+      \n2. Прочитать из файла строку"
+
+input_choice = gets.chomp
+
+case input_choice
+
+  when "1"
+    puts "Введите целые числа через пробел:"
+    input_data = gets.chomp.split.map(&:to_i)
+
+  when "2"
+    begin
+      puts "Введите путь к файлу:"
+
+      file_path = gets.chomp
+      file = File.open(file_path, "r")
+      input_data = file.read.split.map(&:to_i)
+
+      puts "Файл подключен успешно!"
+      rescue Errno::ENONET => error
+      puts "Обнаружена ошибка при работе с файлами: #{error}"
+      exit
+
+      ensure
+      file.close if file
+      end
+
+  else
+    puts "Неверный выбор."
+    exit
+end
+
+puts 
+puts "Выберите задачу:
+        \n1. Переставить в обратном порядке элементы массива, расположенные 
+        между его минимальным и максимальным элементами."
+
+method_choice = gets.chomp
+
+puts
+
+case method_choice
+
+  when "1"
+    puts reverse_between_min_and_max(input_data) {|min_index, max_index| (input_data[0...min_index + 1] + input_data[min_index + 1...max_index].reverse + input_data[max_index..-1])}.inspect
+  else
+    puts "Некорректный выбор задачи."
+
+end
+
+
